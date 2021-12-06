37b53c2945c03866d20cb20e8e413deafbef3d4e
Eidur Alfredsson <24564968+eiduralfredsson@users.noreply.github.com>
2021-12-06 17:34:31 +0000

    day 1 part 2 added

---
+++ 2021/day1/day1part2.hs
@@ -0,0 +1,47 @@
+module Day1Part2 where
+
+-- Recursive
+getIncCount :: [Int] -> Int
+getIncCount [] = 0
+getIncCount [_] = 0
+getIncCount (x:x':xs)
+  | x' > x    = 1 + getIncCount (x':xs)
+  | otherwise = getIncCount (x':xs)
+
+-- List comprehension
+getIncCount' :: [Int] -> Int
+getIncCount' [] = 0
+getIncCount' [_] = 0
+getIncCount' xs = length [y | (x, y) <- zip xs $ tail xs, y > x]
+
+-- sum over zipWith
+getIncCount'' :: [Int] -> Int
+getIncCount'' [] = 0
+getIncCount'' [_] = 0
+getIncCount'' xs = sum $ zipWith (\x y -> if (y>x) then 1 else 0) xs $ tail xs
+
+-- build a three-measurement sliding window
+mkWindow :: [Int] -> [(Int,Int,Int)]
+mkWindow xs@(x:x':x'':x''') = (x,x',x'') : (mkWindow . tail) xs
+mkWindow _             = []
+
+-- sum the triples in a sliding window
+sumWindow :: [(Int,Int,Int)] -> [Int]
+sumWindow xs = map (\(x,y,z) -> x+y+z) xs
+
+-- Single is for day 1 part 1, Window is for day 1 part 2
+data ReportType = Single | Window
+
+-- Output action. Takes the file path, the counting funtion (one of the above).
+-- Example: report "input.txt" getIncCount''
+report :: String -> ([Int] -> Int) -> IO ()
+report path f = do
+  content <- readFile path
+  let measurements = map read $ lines content :: [Int]
+  putStr "Part 1 answer:"
+  print $ f measurements
+  putStr "Part 2 answer:"
+  print $ (f . sumWindow . mkWindow) measurements
+
+
+
