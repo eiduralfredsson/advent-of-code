1fd2dd00817690c31fdc04ab5303305c8dd982df
Eidur Alfredsson <24564968+eiduralfredsson@users.noreply.github.com>
2021-12-11 10:09:19 +0000

    day 7 part 2

---
+++ 2021/day7/day7part2.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Day7Part2 where
+
+import qualified Data.Text as T
+
+type Crab = Int
+type Target = Int
+type Fuel = Int
+
+-- Get a list of all possible targets
+targets :: [Crab] -> [Target]
+targets cs = [minimum cs .. maximum cs]
+
+-- Calculate fuel cost of moving all crabs to given target
+fuel :: [Crab] -> Target -> Fuel
+fuel cs t = sum $ map (\x -> sum [1 .. abs $ t - x]) cs     -- CHANGED for part 2
+
+-- Get a list of all possible fuel costs
+allFuel :: [Crab] -> [Target] -> [Fuel]
+allFuel cs = map (fuel cs)
+
+-- Get optimal fuel number
+minFuel :: [Fuel] -> Fuel
+minFuel = minimum
+
+
+report :: String -> IO ()
+report path = do
+  content <- readFile path
+  let cs = map (read . T.unpack) $ T.splitOn "," $ T.strip $ T.pack content :: [Crab]
+  let ts = targets cs
+  let fs = allFuel cs ts
+  let res = minFuel fs
+  print res
